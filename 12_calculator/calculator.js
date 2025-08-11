95c7a99a95c06eb599dfb7cf8c3a7e77de6f7804
mtcho <myintthwaycho11@gmail.com>
2025-08-11 11:06:12 +0700

    solve exe 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  
+	return arr.reduce((sum,num)=>{
+    return sum + num;
+  },0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prod,num)=>{
+    return prod * num;
+  },1);
 };
 
-const power = function() {
-	
+const power = function(num,times) {
+  let product = 1;
+	for (let i =1;i<=times;i++){
+    product = product * num;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if( num ==0 ){
+    return 1;
+  }
+	if( num ==1 ){
+    return 1;
+  }
+  if ( num >1){
+    return num * factorial(num-1);
+  }
 };
 
 // Do not edit below this line
