9fcbe6ce900d97dd08c2868b27b10a146e57c6b0
mtcho <myintthwaycho11@gmail.com>
2025-08-11 11:23:10 +0700

    solve exe 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let cleaned = text.replace(/[^a-z0-9]/gi, "");//onjly keeps letters and numbers
+    let lowerCase = cleaned.toLowerCase();
+    let reversed = "";
+    for (let i = lowerCase.length - 1; i >= 0; i--) {
+        reversed += lowerCase[i];
+    }
+    if (reversed == lowerCase) return true;
+    else return false;
 };
 
 // Do not edit below this line
