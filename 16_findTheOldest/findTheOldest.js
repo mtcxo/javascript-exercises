9ddb30203a49a8e75e97637e44b159bbe6aedd04
mtcho <myintthwaycho11@gmail.com>
2025-08-11 13:08:24 +0700

    solve exe 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+    return arr.reduce((oldest, current) => {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return oldestAge < currentAge ? current : oldest;
+    })
 };
 
+function getAge(birthYear, deathYear) {
+    if (!deathYear) {
+        deathYear = new Date().getFullYear();
+    }
+    return deathYear - birthYear;
+}
+
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+
+console.log(findTheOldest(people));    
 // Do not edit below this line
 module.exports = findTheOldest;
